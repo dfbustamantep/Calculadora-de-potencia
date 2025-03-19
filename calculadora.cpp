b76c6d80464e14b869b46eae6e9296a688e22f5c
Daniel Bustamante <vtdanielf@gmail.com>
2025-03-19 18:18:29 -0500

    Calculo de la potencia en dBw

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,12 +1,53 @@
 #include<iostream>
+#include <math.h>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
+class Calculadora
+{
+private:
+    float a,b,c;
+public:
+    Calculadora(float a,float b,float c);
+    Calculadora();
+    ~Calculadora();
+    float convertirMwaW(float n);
+    float calcular();
+};
+
+Calculadora::Calculadora(float a,float b,float c)
+{
+    this->a=a;
+    this->b=b;
+    this->c=c;
+}
+
+Calculadora::Calculadora()
+{
+    a=0;
+    b=0;
+    c=0;
+}
+
+Calculadora::~Calculadora()
+{
+}
+
+float Calculadora::convertirMwaW(float n){
+    return n/1000;
+}
+
+float Calculadora::calcular(){
+    float dbW;
+    dbW =a+(b+c);
+//    w=(pow(10,))
+    return dbW;
+}
 main(){
     float a=0,b=0,c=0;
-    cout<<"Ingrese el valor de B en wats"<<endl;
+    cout<<"Ingrese el valor de B en decibell wats"<<endl;
     cin>>b;
 
     cout<<"Ingrese el valor de A en decibell wats"<<endl;
@@ -15,5 +56,11 @@ main(){
     cout<<"Ingrese el valor de C en decibells"<<endl;
     cin>>c;
 
+    // Crear un objeto de la clase Calculadora
+    Calculadora calc(a, b, c);
+
+
+    cout<<"La potencia es de: "<<calc.calcular()<<" dBW"<<endl;
 
 }
+
