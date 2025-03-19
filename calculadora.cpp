1a3c7641b306f38e0c08ff31c904ee925948b3ea
Daniel Bustamante <vtdanielf@gmail.com>
2025-03-18 22:52:50 -0500

    definir entradas

---
+++ calculadora.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+main(){
+    float a=0,b=0,c=0;
+    cout<<"Ingrese el valor de B en wats"<<endl;
+    cin>>b;
+
+    cout<<"Ingrese el valor de A en decibell wats"<<endl;
+    cin>>a;
+
+    cout<<"Ingrese el valor de C en decibells"<<endl;
+    cin>>c;
+
+
+}
